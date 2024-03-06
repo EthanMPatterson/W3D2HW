8803fc17f9c4dac0190301b5957dac7df2f42528
Ethan Patterson <159935067+EthanMPatterson@users.noreply.github.com>
2024-03-05 22:50:36 -0500

    Initial Commit

---
+++ geometry.py
@@ -0,0 +1,11 @@
+#Feels like a cheap way out importing math into this lol not sure if I should have made my own.
+import math
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+def hypotenuse(side1, side2):
+    return math.sqrt(side1 ** 2 + side2 ** 2)
+
+
+
